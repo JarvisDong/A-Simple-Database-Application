3679996a9016f372262cb3c89559e5caff1bb5e6
JarvisDong <jarvishdong@gmail.com>
2018-12-04 17:13:42 -0800

    first commit

---
+++ Final Project Part I.md
@@ -0,0 +1,20 @@
+###Final Project Part I
+
+CIS451, Fall 2018 
+
+Jarvis Dong
+
+------
+
+This is a ER diagram of my database for final project. Though it's not fictional, it may not designed for actual use.
+
+- Each student has a unique identifying number, fname, lname. Student may not declare a major
+- Each class has a unique CRN number, class name, textbook for homework, and a classroom.
+- Each professor has a unique id, fname, lname and a major.
+- Each department has a unique major, name, building name, and telephone number.
+- A professor must work at a department. A student may register in a department, which means declaring a major.
+- A professor teaches a class and a class may be conducted by more than one professor
+- A student can be a GTF who works for and gets paid from department. GTFs are signed to assist professors.
+- A class can be taken by more than one student and need to be managed by department.
+
+![image-20181114163716777](/Users/haojun/Library/Application Support/typora-user-images/image-20181114163716777.png)
---
+++ PHP/findProfessor.php
@@ -0,0 +1,57 @@
+<?php
+
+include('./connectionData.txt');
+
+$conn = mysqli_connect($server, $user, $pass, $dbname, $port)
+or die("Error connecting to MySQL server.");
+
+?>
+
+<html>
+<head>
+
+<h2>Student Information Search</h2>
+</head>
+<body bgcolor="white">
+
+<p>Type a professor last name to search his/her information</p>
+
+<p>Note: </p>
+<p>The form requires a last name as input</p>
+
+<hr>
+
+<?php
+    if ($mysqli->connet_errno) {
+        echo "<h2>Failed to connect to MySQL: (" . $mysqli->connect_errno . ") ". $mysqli->connect_error . "<h2>";
+    }
+
+    $lname = $_POST['lname'];
+
+    $lname = mysqli_real_escape_string($conn, $lname);
+
+    #TODO: add proper query
+    $query = "select, fname, lname, email, major, office from professor where lname = ";
+    $query = $query."'".$lname."' ORDER BY fname;";
+
+?>
+
+<hr>
+<p>Result of searching:</p>
+
+<?php
+$result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+print "<pre>";
+while ($row = mysqli_fetch_array($result, MYSQL_BOTH)) {
+    print "\n";
+    print "$row[fname] $row[lname] $row[email] $row[major] $row[office]";
+}
+print "</pre>";
+
+mysqli_free_result($result);
+mysqli_close($conn);
+?>
+
+</body>
+</html>
