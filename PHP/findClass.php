16d8ae6a0b0f24ee2de138edb37582731eefcc5d
JarvisDong <jarvishdong@gmail.com>
2018-12-05 21:45:33 -0800

    sdsd

--- PHP/findClass.php
+++ PHP/findClass.php
@@ -36,7 +36,7 @@ or die("Error connecting to MySQL server.");
 ?>
 
 <hr>
-<p>Result of searching:</p>
+<p>Result  bbbbbbb of searching:</p>
 
 <?php
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
